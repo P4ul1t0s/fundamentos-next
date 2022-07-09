7d888e62961a7bb5b7f8fe3e2a7467a0ed5a4e8c
Paulo <paulomafra2004@gmail.com>
2022-07-09 12:50:00 -0300

    CSS modularizado

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/estiloso.module.css'
+
+export default function Estiloso(){
+    return(
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS modules</h1>
+        </div>
+    )
+}
