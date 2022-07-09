072bb9566ee7fe74ab9b9534aa47b16287eb7f86
Paulo <paulomafra2004@gmail.com>
2022-07-09 14:37:31 -0300

    Navegação entre componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,8 +1,10 @@
+import Link from 'next/link'
 import styles from '../styles/estiloso.module.css'
 
 export default function Estiloso(){
     return(
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estilo usando CSS modules</h1>
         </div>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,9 @@
+import Link from "next/link"
+
 export default function Inicio(){
-  return <h1>Título</h1>
+    return(
+        <div>
+            <Link href="/estiloso">Estiloso</Link>
+        </div>
+    )
 }
