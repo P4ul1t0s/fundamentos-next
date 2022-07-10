34ac79a8b7c0ef6dbaa9ba8a20a72f4438ea34f4
Paulo <paulomafra2004@gmail.com>
2022-07-09 22:07:08 -0300

    Navegação dinâmica

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,23 @@ import Navegador from "../components/Navegador"
 
 export default function Inicio(){
     return(
-        <div style={{
-            display: 'flex',
+        <main style={{
             justifyContent: 'center',
             alignItems: 'center',
             height: '100vh',
-            flexWrap: 'wrap'
+            display: 'flex'
         }}>
-            <Navegador texto="Estiloso" destino="estiloso"/>
-            <Navegador texto="Exemplo" destino="exemplo" cor="darkviolet"/>
-            <Navegador texto="JSX" destino="jsx" cor="crimson"/>
-            <Navegador texto="Navegação #01" destino="navegacao" cor="green"/>
-        </div>
+            <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center'
+            }}>
+                <Navegador texto="Estiloso" destino="estiloso"/>
+                <Navegador texto="Exemplo" destino="exemplo" cor="darkviolet"/>
+                <Navegador texto="JSX" destino="jsx" cor="crimson"/>
+                <Navegador texto="Navegação #01" destino="navegacao" cor="green"/>
+                <Navegador texto="Navegação #02" destino="cliente/sp-2/321" cor="blue"/>
+            </div>
+        </main>
     )
 }
