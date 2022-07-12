9c5b252635cf48a7864385a15326a43a51d8fde2
Paulo <paulomafra2004@gmail.com>
2022-07-12 09:27:40 -0300

    Pré-Renderização

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,6 +20,7 @@ export default function Inicio(){
                 <Navegador texto="Navegação #02" destino="cliente/sp-2/321" cor="blue"/>
                 <Navegador texto="Componente com Estado" destino="estado" cor="pink"/>
                 <Navegador texto="Integração com API #01" destino="integracao_1" cor="orange"/>
+                <Navegador texto="Conteúdo Estático" destino="estatico" cor="purple"/>
             </div>
         </main>
     )
